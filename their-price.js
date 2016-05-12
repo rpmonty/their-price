6fd1b2dd43c5b4e50476e33bd9edddac0960db19
rpmonty <rpmontville@gmail.com>
2016-05-12 12:36:56 -0400

    Update their-price.js

--- their-price.js
+++ their-price.js
@@ -1,7 +1,16 @@
-jQuery('.vehicle .theirPrice .price').each(function() {
-  
-  var amountToAdd = -500;
-  var start_price = parseInt(jQuery(this).text().replace(/\$|,/g, ''));
 
-  jQuery(this).text("$" + parseInt(amountToAdd + start_price).toLocaleString());
+jQuery("body").live("vrp-ready vrp-ajax-complete", function() {
+jQuery('.new-vehicle .our-price .price').each(function() {
+  var disc= 500;
+  var start_price = parseInt(jQuery(this).text().replace(/[^0-9.]/g, ''));
+
+  jQuery(this).text("$" + parseInt(start_price - disc).toLocaleString());
+});
+jQuery('.new-vehicle .discounts .price').each(function() {
+  var moredisc= 500;
+  var start_disc = parseInt(jQuery(this).text().replace(/[^0-9.]/g, ''));
+
+  jQuery(this).text("$" + parseInt(start_disc + moredisc).toLocaleString());
+});
+
 });
